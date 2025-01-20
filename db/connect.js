16845462b6252c1fe6bea087b06c3650228c8960
Alvin <alvin.chen.718@gmail.com>
2025-01-20 18:39:47 -0500

    Add pg

---
+++ db/connect.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const pgp = require('pg-promise')(/* options */);
+const db = pgp(process.env.DATABASE_URL);
+
+db.one('SELECT $1 AS value', 123)
+  .then((data) => {
+    console.log('DATA:', data.value);
+  })
+  .catch((error) => {
+    console.log('ERROR:', error);
+  });
