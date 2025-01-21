f3ac2efb20879a7fc071f32f968366c5dd1a4596
Alvin <alvin.chen.718@gmail.com>
2025-01-20 20:02:32 -0500

    Expose endpoint

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const productRouter = require('./routes/products');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/v1', productRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
